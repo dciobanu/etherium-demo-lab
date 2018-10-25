35f8eb821b00eb1fb6d535a94385c7b0d683362f
Dorin Ciobanu <dorin@ciobanu.org>
2018-10-25 14:57:08 -0700

    WIP: Lab4 - Only allow setEmployee calls from the owner

--- employee.sol
+++ employee.sol
@@ -1,21 +1,31 @@
 pragma solidity ^0.4.18;
 
 contract Employee {
-    
-    string fName;
-    uint age;
+
+    string  fName;
+    uint    age;
+    address owner;
 
     event EmployeeEvent (
         string name,
         uint age
     );
 
-    function setEmployee(string _fName, uint _age) public {
+    function Employee() public { // constructor
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function setEmployee(string _fName, uint _age) onlyOwner public {
         fName = _fName;
         age = _age;
         EmployeeEvent(fName, age);
     }
-   
+
     function getEmployee() view public returns (string, uint) {
         return (fName, age);
     }
