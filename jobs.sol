1e78d8af1c8eab9b4c9203423f3808c0275cea07
Dorin Ciobanu <dorin@ciobanu.org>
2018-10-26 10:07:34 -0700

    lab6: Changing string to bytes

--- jobs.sol
+++ jobs.sol
@@ -6,25 +6,31 @@ contract Owned {
     function Owned() public {
         owner = msg.sender;
     }
-    
-   modifier onlyOwner {
+
+    modifier onlyOwner {
        require(msg.sender == owner);
        _;
    }
 }
 
-contract Jobs is Owned{
+contract Jobs is Owned {
     
     struct Employee {
         uint age;
-        string fName;
-        string lName;
+        bytes16 fName;
+        bytes16 lName;
     }
-    
+
     mapping (address => Employee) employees;
     address[] public employeeAccts;
-    
-    function setEmployee(address _address, uint _age, string _fName, string _lName) public {
+
+    event employeeInfo(
+       bytes16 fName,
+       bytes16 lName,
+       uint age
+    );
+
+    function setEmployee(address _address, uint _age, bytes16 _fName, bytes16 _lName) onlyOwner public {
         var employee = employees[_address];
         
         employee.age = _age;
@@ -32,18 +38,18 @@ contract Jobs is Owned{
         employee.lName = _lName;
         
         employeeAccts.push(_address) -1;
+        employeeInfo(_fName, _lName, _age);
     }
-    
+
     function getEmployees() view public returns(address[]) {
         return employeeAccts;
     }
-    
-    function getEmployee(address _address) view public returns (uint, string, string) {
+
+    function getEmployee(address _address) view public returns (uint, bytes16, bytes16) {
         return (employees[_address].age, employees[_address].fName, employees[_address].lName);
     }
 
     function countEmployees() view public returns (uint) {
         return employeeAccts.length;
     }
-
 }
